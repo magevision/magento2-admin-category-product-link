f85ec1207b056a9d66951667cc0b67274435bac9
Nikolas <nikolakisae@gmail.com>
2023-03-16 21:34:39 +0200

    Compatibility update 2.4.6

--- Observer/AdminhtmlBlockHtmlBeforeObserver.php
+++ Observer/AdminhtmlBlockHtmlBeforeObserver.php
@@ -5,17 +5,18 @@
  * @category     MageVision
  * @package      MageVision_AdminCategoryProductLink
  * @author       MageVision Team
- * @copyright    Copyright (c) 2022 MageVision (https://www.magevision.com)
+ * @copyright    Copyright (c) 2023 MageVision (https://www.magevision.com)
  * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 declare(strict_types=1);
 
 namespace MageVision\AdminCategoryProductLink\Observer;
 
-use Magento\Framework\Event\ObserverInterface;
+use Exception;
+use Magento\Catalog\Block\Adminhtml\Category\Tab\Product;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\Observer;
-use Magento\Catalog\Block\Adminhtml\Category\Tab\Product;
+use Magento\Framework\Event\ObserverInterface;
 
 class AdminhtmlBlockHtmlBeforeObserver implements ObserverInterface
 {
@@ -40,7 +41,7 @@ class AdminhtmlBlockHtmlBeforeObserver implements ObserverInterface
      *
      * @param Observer $observer
      * @return void
-     * @throws \Exception
+     * @throws Exception
      */
     public function execute(Observer $observer)
     {
