bfc72322621e97373eea6c354cfb175a79786229
Nikolas <nikolakisae@gmail.com>
2024-04-11 18:25:54 +0200

    Compatibility update

--- Observer/AdminhtmlBlockHtmlBeforeObserver.php
+++ Observer/AdminhtmlBlockHtmlBeforeObserver.php
@@ -2,11 +2,11 @@
 /**
  * MageVision Admin Category Product Link Extension
  *
- * @category     MageVision
- * @package      MageVision_AdminCategoryProductLink
- * @author       MageVision Team
- * @copyright    Copyright (c) 2024 MageVision (https://www.magevision.com)
- * @license      http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @category  MageVision
+ * @package   MageVision_AdminCategoryProductLink
+ * @author    MageVision Team
+ * @copyright Copyright (c) 2024 MageVision (https://www.magevision.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 declare(strict_types=1);
 
@@ -34,7 +34,7 @@ class AdminhtmlBlockHtmlBeforeObserver implements ObserverInterface
     /**
      * Add edit product column
      *
-     * @param Observer $observer
+     * @param  Observer $observer
      * @return void
      * @throws Exception
      */
